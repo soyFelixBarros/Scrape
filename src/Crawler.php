f20eb2107068203370612ecb5911fa3ca62f232f
Felix Barros <soyFelixBarros@gmail.com>
2018-01-31 15:21:28 -0300

    new chaned

--- src/Crawler.php
+++ src/Crawler.php
@@ -20,6 +20,7 @@ class Crawler
 
     /**
      * Obtener el contenido HTML de una página.
+     * 
      * @param string $url Url de la página a scrapear
      * @return string
      */
---
+++ tests/UrlTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class UrlTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetUrl()
+    {
+    }
+}
