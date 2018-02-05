9938ea6c0b94f5f24dbd8f1fd2f7f45e53c3712d
Felix Barros <soyFelixBarros@gmail.com>
2018-02-05 12:32:18 -0300

    styleci 8

--- src/Str.php
+++ src/Str.php
@@ -5,12 +5,12 @@ namespace Felix\Scraper;
 class Str
 {
 	/**
-	 * Eliminar espacios en blanco de una cadena.
-	 * 
-	 * @param  string $str Cada de texto.
-	 *
-	 * @return string
-	 */
+	* Eliminar espacios en blanco de una cadena.
+	* 
+	* @param  string $str Cada de texto.
+	*
+	* @return string
+	*/
 	public static function remove_spaces($str)
 	{
 		$str = html_entity_decode($str);
@@ -21,26 +21,26 @@ class Str
 	}
 
 	/**
-	 * Eliminar multiples espacios en blanco entre dos cadenas.
-	 * 
-	 * @param  string $str Cada de texto.
-	 * 
-	 * @return string
-	 */
+	* Eliminar multiples espacios en blanco entre dos cadenas.
+	* 
+	* @param  string $str Cada de texto.
+	* 
+	* @return string
+	*/
 	public static function remove_multi_spaces($str)
 	{
 		$str = preg_replace('!\s+!', ' ', $str);
-		
+
 		return $str;
 	}
-	
+
 	/**
-	 * Limpiar una cadena de caracteres.
-	 * 
-	 * @param string $str Cadena de caracteres a limpiar. 
-	 * 
-	 * @return string
-	 */
+	* Limpiar una cadena de caracteres.
+	* 
+	* @param string $str Cadena de caracteres a limpiar. 
+	* 
+	* @return string
+	*/
 	public static function clean($str)
 	{
 		$str = self::remove_spaces($str);
